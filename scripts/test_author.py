7606f575591ffbd1341568c277a5ed1ec0715673
Wences91 <wences91@gmail.com>
2019-10-22 17:44:28 +0200

    Update test_author.py

--- scripts/test_author.py
+++ scripts/test_author.py
@@ -1,11 +1,10 @@
 from worldcatidentities import Authority, AuthorityData
 
-author = AuthorityData('Federico Gracía Lorca')
+author = Authority('Federico Gracía Lorca')
 author.search()
 author.fixed_name
-author.data()
-author.languages
-author = AuthorityWorks('Federico Gracía Lorca')
+
+author = AuthorityData('Federico Gracía Lorca')
 author.data()
 author.works
 author.name
