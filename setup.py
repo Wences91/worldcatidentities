81eee78d551c9e205b1acbc509e7cc73a38ee844
Wences91 <wences91@gmail.com>
2019-10-22 10:48:32 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name = 'worldcatidentities',
+  packages = ['worldcatidentities'],
+  version = '0.1',
+  license='GPL-3',
+  description = 'Package that recovers authorities data from OCLCs WorldCat Identities API',
+  author = 'Wenceslao Arroyo-Machado',
+  author_email = 'wences@ugr.es',
+  url = 'https://github.com/Wences91/worldcatidentities',
+  download_url = 'https://github.com/Wences91/worldcatidentities',
+  keywords = ['API', 'WorldCat Identities'],
+  install_requires=[
+          'validators'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GPL-3',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
