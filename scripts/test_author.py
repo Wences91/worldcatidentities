badf63350446ac5e203c4a0c4cf039bada134b0d
Wences91 <wences91@gmail.com>
2019-11-20 20:48:42 +0100

    Update test_author.py

--- scripts/test_author.py
+++ scripts/test_author.py
@@ -4,12 +4,17 @@ author = Authority('Federico Gracía Lorca')
 author.search()
 author.fixed_name
 
-author = AuthorityData('Federico Gracía Lorca')
+author = AuthorityData(name = 'Federico Gracía Lorca')
+author.data()
+author.works
+author.name
+
+author = AuthorityData(uri = 'lccn-n79034425')
 author.data()
 author.works
 author.name
 
 for i in ['Federico Gracía Lorca', 'Virginia Woolf']:
-    authority = AuthorityData(i).data()
+    authority = AuthorityData(name = i).data()
     print(authority.name)
     print(authority.works)
