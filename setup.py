8369b523113d23f2cb32689a30a38e93887fe6a1
Wences91 <wences91@gmail.com>
2019-11-26 13:21:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class CleanCommand(Command):
 setup(
   name = 'worldcatidentities',
   packages = find_packages(exclude=["tests"]),
-  version = '0.5',
+  version = '0.5.8',
   license='GPL-3',
   description = 'Package that recovers authorities data from OCLCs WorldCat Identities API',
   author = 'Wenceslao Arroyo-Machado',
